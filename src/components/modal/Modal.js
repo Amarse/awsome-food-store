d2498f3996fec8a9520e00f67bc02f62da66ce1c
Amarse <byicy08@gmail.com>
2022-10-19 00:02:02 +0900

    changed props

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,13 +1,11 @@
-import {  Modal } from 'semantic-ui-react';
+import { Modal } from 'semantic-ui-react';
 
 export default function DetailModal(props) {
-
+  const { onClose } = props;
   return (
     <>
-      <Modal closeIcon style={{top: "20%"}}
-        open={true}
-        onClose={props.onClose}>
-          <Modal.Header></Modal.Header>
+      <Modal closeIcon style={{ top: '20%' }} open={true} onClose={onClose}>
+        <Modal.Header></Modal.Header>
         <Modal.Content>{props.children}</Modal.Content>
       </Modal>
     </>
--- src/components/storelist/StoreListItem.js
+++ src/components/storelist/StoreListItem.js
@@ -1,16 +1,11 @@
 import Link from 'next/link';
 import { Image } from 'semantic-ui-react';
 
-export default function StoreListItem({ id, thumb }) {
-  
+export default function StoreListItem(props) {
+  const { id, thumb } = props;
   return (
     <>
-      <Link
-      href={`/store?id=${id}`}
-        as={`/store/${id}`}
-        scroll={false}
-        shallow={true}
-      >
+      <Link href={`/store?id=${id}`} as={`/store/${id}`} scroll={false} shallow={true}>
         <a>
           <Image src={thumb} rounded size="medium" style={{ margin: '20px 0 ' }} />
         </a>
